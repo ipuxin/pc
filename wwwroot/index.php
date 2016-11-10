3a3b87b78c1d6ac4a46e2b0baf6709b21738309d
yuChao <ipuxin@qq.com>
2016-11-10 10:41:22 +0800

    添加测试模块

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,4 +1,21 @@
  <?php
+require_once (__DIR__ .'/../common/kint/Kint.class.php');
+//ddd($_SERVER);
+ /**
+  * 跟踪
+  */
+// $a = 1;$b=3;
+// Kint::trace();
+ //文本输出
+// s($a);
+ //输出文本后,终止命令
+// sd();
+ /**
+  * 会清除在这之前的所有文字类型的输出
+  */
+// -d($a);
+ //输出的结果自动展开,无需手动点击.
+// !d($a);
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
